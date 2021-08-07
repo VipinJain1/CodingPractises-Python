9ca60ac03fa12fa2e4e5cc8c44ecf233fb980e00
Vipin Jain <vipjain@gmail.com>
2021-08-07 17:49:01 -0400

    HackerRank code

--- HackerRank_Bill_Division.py
+++ HackerRank_Bill_Division.py
@@ -9,15 +9,17 @@ def bonAppetit(bill, k, b):
     # Write your code here
     
     total  = round (abs(sum (bill) - bill[1])/2)
+    if total == 0:
+        return 0
     x = int (b - total)
-    if x ==0:
+    if x <=0:
         print ('Bon Appetit')
     else:
         print (int (b - total))
 
-
 bill =[3,10,2,9]
+bill = [2,0,0,0]
 k =1
-b =12
+b =0
 
 bonAppetit(bill,k,b)
