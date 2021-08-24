9a9f378f8daba07e5792ee246477428c70a39ef7
Vipin Jain <vipjain@gmail.com>
2021-08-24 14:20:26 -0400

    HackerRank code

---
+++ HackerRank_Height_of_a_Binary_Tree.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 24 14:19:49 2021
+
+https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem
+
+@author: VIP
+"""
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+def height(root):
+    lh =0
+    lr =0
+    
+    if root.left != None:
+        lh =  1 + height(root.left)
+     
+    if root.right != None:
+        lr = 1 + height(root.right)
+    
+    if lh> lr:
+        return (lh)
+    else:
+        return (lr)
+     
