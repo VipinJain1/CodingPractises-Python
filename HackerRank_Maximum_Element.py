525653818019f850cfd370c3f3340d61c69f4a05
Vipin Jain <vipjain@gmail.com>
2021-08-24 23:21:47 -0400

    HackerRank code

---
+++ HackerRank_Maximum_Element.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 24 22:51:27 2021
+
+@author: VIP
+"""
+
+
+
+
+def getMax(operations):
+    # Write your code here
+    #print (operations)
+    A  = []
+    results = []
+    for i in operations:
+        data = i.split(' ')
+        if data[0] == '1':
+            A.extend(data[1:])
+        elif  data[0] == '2':
+            del A[len(A) -1]
+        elif  data[0] == '3':
+            results.append (max(A))
+    return results
+        
+operations = ['1 97', '2', '1 20', '2', '1 26', '1 20', '2', '3', '1 91', '3']
+
+print (getMax(operations))
