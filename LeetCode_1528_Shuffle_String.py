fdf1cc2d99414bb8666b76040b54e7ed0e87d4f4
Vipin Jain <vipjain@gmail.com>
2021-08-30 18:22:45 -0400

    Leet code

--- LeetCode_1528_Shuffle_String.py
+++ LeetCode_1528_Shuffle_String.py
@@ -6,22 +6,13 @@ Created on Mon Aug 30 15:41:46 2021
 """
 
 
-
-def restoreString(s, indices):
-        ln = len(s)
-        if ln ==0:
-            return None
-        
-        if len(indices) ==0:
-            return None
-        count =0
-        s1 = list (s)
-        for i in indices:
-            s1[count], s1[i] = s1[i], s1[count]
-            count = count +1
-        return s1 
-    
-
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res = [0 for i in range(len(s))]
+        for i in range(len(s)):
+            res[indices[i]] = s[i]
+            
+        return ''.join(res)
 s = "codeleet"
 indices = [4,5,6,7,0,2,1,3]
 print (restoreString(s, indices))
