575902ca41956397a41c471206a695f9bc7b7923
Vipin Jain <vipjain@gmail.com>
2021-08-31 11:40:27 -0400

    Leet code

---
+++ LeetCode_709_To Lower Case.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 31 11:40:03 2021
+
+@author: VIP
+"""
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+        
+        
