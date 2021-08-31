30d23b39e8beba533232c2f78a17fe75618a533b
Vipin Jain <vipjain@gmail.com>
2021-08-30 23:44:23 -0400

    Leet code

--- LeetCode_219_Contains_DuplicateII.py
+++ LeetCode_219_Contains_DuplicateII.py
@@ -5,6 +5,7 @@ Created on Mon Aug 30 23:00:46 2021
 @author: VIP
 """
 
+"""
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         
@@ -20,6 +21,35 @@ class Solution:
                     found = True
         return found
     
-                    
+"""
+
+
+                  
+
+        
+def containsNearbyDuplicate(nums,k):
+    d = dict()
+    found = False
+    for count, i in enumerate (nums):
+        if i in d.keys():
+            d[i].append(count)
+        else:
+            d[i] = [count]
+    for i in d.values():
+        dist = i[0]
+        if len(i) >1:
+            for j in range (1,len(i)):
+                if 
             
+                
             
+                found = True
+                return found
+    return found
+        
+
+    
+nums =[1,2,3,1]
+k =3
+print (containsNearbyDuplicate(nums,k)) 
+   
