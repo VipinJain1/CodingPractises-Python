67416dd3593d0b3e8ea1dd4de2a3f621772d3297
Vipin Jain <vipjain@gmail.com>
2021-09-08 10:13:49 -0400

    Leet code

--- LeetCode_179. Largest Number.py
+++ LeetCode_179. Largest Number.py
@@ -5,34 +5,42 @@ Created on Wed Sep  8 01:36:09 2021
 @author: VIP
 """
 
-def largestNumber(nums):
+def largestNumber(A):
     ln = len(nums)
     if ln ==0:
         return 0
     if ln ==1:
-        return nums
-    nums1 = sorted(nums)[::-1]
-    s = str()
+        return str(nums[0])
+    if all([ v == 0 for v in nums ]):
+        return "0"
+        
     dct ={}
-    for i in nums1:
+    for i in nums:
         dgt = int (str(i)[0])
-        print (dgt)
+
         if dgt not in dct.keys():
             dct[dgt] = [i]
         else:
             dct[dgt].append(i)
-    
-    
+
+
     st = str()
     cnt =9
     while (cnt>=0):
         if cnt in dct.keys():
-            st = st + "".join([str(elm) for elm in sorted(dct[cnt])[::-1]])
+            data = sorted(list (map(str,dct[cnt])))[::-1]
+            if len(data) >=2:
+                for i in range (0, len(data) -1):
+                    if int(data[i] + data[i+1]) <  int(data[i+1] + data[i]):
+                        data[i],data[i+1] = data[i+1], data[i]
+
+            lst = "".join([elm for elm in data])
+            st = st + lst
             cnt = cnt -1
         else:
             cnt = cnt-1
     return st    
-        
-        
+
 nums = [3,30,34,6,9]
+nums  = [10]
 print (largestNumber(nums))
