8d850938c2dafe6331166fd6ec71b8cd959257e6
Vipin Jain <vipjain@gmail.com>
2021-11-25 10:52:52 -0500

    LeetCode

---
+++ LeetCode_253_Meeting Rooms II.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 25 10:51:41 2021
+
+@author: VIP
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        ln = len(intervals)
+        #start=0
+        
+        start = [i[0] for i in intervals]
+        end =   [i[1] for i in intervals]
+        start.sort()
+        end.sort()
+        print (start)
+        print (end)
+        
+        s =0
+        res  = 0
+        cnt = 0
+        e =0
+        while (s <ln):
+            if start[s] < end[e]:
+                cnt +=1
+                s +=1
+            else:
+                cnt -=1
+                e +=1
+               
+            res  = max(cnt,res)
+        return res
+    
+        
+        
+        
