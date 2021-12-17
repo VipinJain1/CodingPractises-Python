b7bee1bbb9b032277394f7bbe0bc33e2b7614a0b
Vipin Jain <vipjain@gmail.com>
2021-12-17 09:16:12 -0500

    Leetcode Update

--- LeetCode_148_Sort List.py
+++ LeetCode_148_Sort List.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep  3 14:05:48 2021
+Spyder Editor
 
-@author: VIP
+This is a temporary script file.
 """
 
 # Definition for singly-linked list.
@@ -12,25 +12,20 @@ Created on Fri Sep  3 14:05:48 2021
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return None
+
+        root = head
+        root1 = head
         
-        root  = head
-        if root == None:
-            return head
-        
-        root1 = root
-        
-        while (root1 != None):
-            root = head
-            while (root != None):
-                if root.next != None:
-                    if root.val > root.next.val:
-                        root.val, root.next.val = root.next.val, root.val
-                root = root.next
-            root1 = root1.next
-            
-        return head
+        while (root):
+            newroot = root.next
+            while  (newroot):
+                if root.val > newroot.val:
+                    root.val, newroot.val = newroot.val, root.val 
                 
-                
+                newroot = newroot.next
+            root = root.next
         
-        
-        
+        return root1
+    
